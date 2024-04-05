17d5c3666d907dc33c04fdd6a631bc95bb9bd1b6
akshaymugale01 <139070784+akshaymugale01@users.noreply.github.com>
2024-04-05 16:39:57 +0530

    CORS update for backend

--- backend/index.js
+++ backend/index.js
@@ -4,14 +4,20 @@ const port = 5000;
 
 const mongoDB = require('./server');
 
-mongoDB()
+mongoDB();
 
+// CORS middleware
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
     );
+    // Handling preflight requests
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+        return res.status(200).json({});
+    }
     next();
 });
 
@@ -22,9 +28,9 @@ app.use('/api', require("./routes/DisplayData"));
 app.use('/api', require("./routes/OrderData"));
 
 app.get('/', (req, res) => {
-    res.send("Hello World!")
-})
+    res.send("Hello World!");
+});
 
 app.listen(port, () => {
-    console.log(`Listening to port: ${port}`)
-})
+    console.log(`Listening to port: ${port}`);
+});
