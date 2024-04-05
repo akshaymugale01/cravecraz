f086d9d60e03af776d5e31cfdeb49e99db8d5d4d
akshaymugale01 <139070784+akshaymugale01@users.noreply.github.com>
2024-04-05 22:59:52 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -1,39 +1,39 @@
-// const express = require('express');
-// const app = express();
-// const port = 5000;
+const express = require('express');
+const app = express();
+const port = 5000;
 
-// const mongoDB = require('./server');
+const mongoDB = require('./server');
 
-// mongoDB();
+mongoDB();
 
-// // CORS middleware
-// app.use((req, res, next) => {
-//     res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-//     res.header(
-//         "Access-Control-Allow-Headers",
-//         "Origin, X-Requested-With, Content-Type, Accept"
-//     );
-//     // Handling preflight requests
-//     if (req.method === 'OPTIONS') {
-//         res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//         return res.status(200).json({});
-//     }
-//     next();
-// });
+// CORS middleware
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    // Handling preflight requests
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+        return res.status(200).json({});
+    }
+    next();
+});
 
-// app.use(express.json());
+app.use(express.json());
 
-// app.use('/api', require("./routes/CreateUser"));
-// app.use('/api', require("./routes/DisplayData"));
-// app.use('/api', require("./routes/OrderData"));
+app.use('/api', require("./routes/CreateUser"));
+app.use('/api', require("./routes/DisplayData"));
+app.use('/api', require("./routes/OrderData"));
 
-// app.get('/', (req, res) => {
-//     res.send("Hello World!");
-// });
+app.get('/', (req, res) => {
+    res.send("Hello World!");
+});
 
-// app.listen(port, () => {
-//     console.log(`Listening to port: ${port}`);
-// });
+app.listen(port, () => {
+    console.log(`Listening to port: ${port}`);
+});
 
 
 // const express = require('express');
@@ -76,39 +76,39 @@
 // });
 
 
-const express = require('express');
-const app = express();
-const port = 5000;
-const axios = require('axios');
+// const express = require('express');
+// const app = express();
+// const port = 5000;
+// const axios = require('axios');
 
-// CORS middleware
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://cravecraz.onrender.com");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    // Handling preflight requests
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-        return res.status(200).json({});
-    }
-    next();
-});
+// // CORS middleware
+// app.use((req, res, next) => {
+//     res.setHeader("Access-Control-Allow-Origin", "");
+//     res.header(
+//         "Access-Control-Allow-Headers",
+//         "Origin, X-Requested-With, Content-Type, Accept"
+//     );
+//     // Handling preflight requests
+//     if (req.method === 'OPTIONS') {
+//         res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//         return res.status(200).json({});
+//     }
+//     next();
+// });
 
-app.use(express.json());
+// app.use(express.json());
 
-// Proxy requests to the API
-app.get('/api/foodData', async (req, res) => {
-    try {
-        const response = await axios.get('https://foodapp-uqfe.onrender.com/api/foodData');
-        res.json(response.data);
-    } catch (error) {
-        console.error('Error:', error);
-        res.status(500).json({ error: 'Internal server error' });
-    }
-});
+// // Proxy requests to the API
+// app.get('/api/foodData', async (req, res) => {
+//     try {
+//         const response = await axios.get('https://cravecrazee.onrender.com/api/foodData');
+//         res.json(response.data);
+//     } catch (error) {
+//         console.error('Error:', error);
+//         res.status(500).json({ error: 'Internal server error' });
+//     }
+// });
 
-app.listen(port, () => {
-    console.log(`Listening to port: ${port}`);
-});
+// app.listen(port, () => {
+//     console.log(`Listening to port: ${port}`);
+// });
